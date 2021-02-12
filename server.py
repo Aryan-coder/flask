faa1d9a31d5bad3e820c64f9ce53ae32280df7a9
Aryan-coder <52967073+Aryan-coder@users.noreply.github.com>
2021-02-12 14:23:25 +0530

    test server

---
+++ server.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+#loading the model
+#model = load_model('mnist_model.h5')
+
+@app.route('/')
+def index():
+    return "<h1>Hello, world!</h1>"
+
+
+if __name__ == '__main__':
+    app.run()
+
